ead89a36761735c3d48d36b75309b2999c8d627e
ikezera <htmtotal@gmail.com>
2023-10-29 02:48:35 +0000

    iamikezera

--- js/padaria.js
+++ js/padaria.js
@@ -1,20 +1,22 @@
 const criar = () => {
+  const inicio = document.getElementById("start");
+  inicio.style.display = "block";
   const criarbtn = document.getElementById("criarmais");
   criarbtn.style.display = "none";
   const tabela1 = document.getElementById("dados");
   const pao1 = document.createElement("img");
   const text1 = document.createElement("h1");
   const texth1 = document.createTextNode(
-    "A Padaria do Tutu é um estabelecimento aconchegante e tradicional que se destaca por oferecer uma variedade de pães frescos, bolos, doces e outros produtos de panificação. Localizada em uma região de atmosfera acolhedora, a padaria é conhecida por seu atendimento amigável e produtos de alta qualidade."
+    "A Padaria do Ike é um estabelecimento aconchegante e tradicional que se destaca por oferecer uma variedade de pães frescos, bolos, doces e outros produtos de panificação. Localizada em uma região de atmosfera acolhedora, a padaria é conhecida por seu atendimento amigável e produtos de alta qualidade."
   );
   pao1.src =
     "https://comper.vteximg.com.br/arquivos/ids/192118-500-500/867535-pao-frances.jpg?v=637635206395630000";
   tabela1.style.width = "100%";
   tabela1.style.height = "31vh";
-  tabela1.style.background = "white";
+  text1.style.fontSize = "2.3rem";
   tabela1.style.display = "flex";
   text1.style.order = "192837";
-  window.scrollBy(600, 1200);
+  window.scrollBy(300, 900);
   text1.appendChild(texth1);
   tabela1.appendChild(text1);
   tabela1.appendChild(pao1);
